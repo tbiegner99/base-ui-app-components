02d9fdf840ba2d5522bea6f36fc4521f58d69366
TJ Biegner <tbiegner99@gmail.com>
2022-01-08 18:28:02 -0500

    update dependencies and app properties

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
